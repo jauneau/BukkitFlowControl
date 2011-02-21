0b19a0fe916572fbafa05acb0ce3e2389440a73b
Vincent Jauneau <vincent.jauneau@platypus-creation.com>
2011-02-21 16:57:23 +0100

    Updated BukkitFlowControl to follow the new code style on JavaPlugin classes.

--- src/com/evilplatypus/bukkit/flow/FlowControl.java
+++ src/com/evilplatypus/bukkit/flow/FlowControl.java
@@ -1,13 +1,9 @@
 package com.evilplatypus.bukkit.flow;
 
-import java.io.File;
 import java.util.logging.Logger;
 
-import org.bukkit.Server;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
-import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -19,14 +15,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FlowControl extends JavaPlugin {
     private final WaterFlowListener waterListener = new WaterFlowListener();
     private final LavaFlowListener lavaListener = new LavaFlowListener();
-    private final Logger log = Logger.getLogger("Minecraft");
-
-	public FlowControl(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
-		super(pluginLoader, instance, desc, folder, plugin, cLoader);
-	}
+    public static Logger log;
 
 	@Override
 	public void onEnable() {
+		log = Logger.getLogger("Minecraft");
 		log.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " loaded.");
 		
         // Register our events
